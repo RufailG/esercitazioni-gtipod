a4c7496595171dd13516fde67e55d0681673a5d4
RufailG <girgis.rufail@iisgalvanimi.edu.it>
2019-11-20 13:36:51 +0000

    bella ciao

---
+++ angular1/src/app/second/second.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'procione',
+  templateUrl: './second.component.html',
+  styleUrls: ['./second.component.css']
+})
+export class SecondComponent implements OnInit {
+ variabile = 'procione';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
